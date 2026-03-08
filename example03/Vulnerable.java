bd0b284db11a406638be6ebcb813c2dfe4326f51
Johnny Nguyen <johnny060904@gmail.com>
2026-03-07 21:04:54 -0500

    add vulnerable code for example03

---
+++ example03/Vulnerable.java
@@ -0,0 +1,8 @@
+public class Vulnerable {
+    public static void main(String[] args) {
+        int[] scores = {90, 85, 88};
+        int index = 5;
+
+        System.out.println("Score: " + scores[index]);
+    }
+}
