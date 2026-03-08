e042a3ceda80a2530c334f0f00927c108e267382
Johnny Nguyen <johnny060904@gmail.com>
2026-03-07 21:14:27 -0500

    add vulnerable code for example06

---
+++ example06/Vulnerable.java
@@ -0,0 +1,9 @@
+public class Vulnerable {
+    public static void main(String[] args) {
+        int total = 100;
+        int count = 0;
+
+        int average = total / count;
+        System.out.println("Average: " + average);
+    }
+}
