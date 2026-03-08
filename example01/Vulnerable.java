95cf3361f78d4a352a53736933bed08e917de921
Johnny Nguyen <johnny060904@gmail.com>
2026-03-07 20:54:06 -0500

    add vulnerable code for example01

---
+++ example01/Vulnerable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Vulnerable {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your age:");
+        int age = input.nextInt();
+
+        System.out.println("Your age is " + age);
+    }
+}
